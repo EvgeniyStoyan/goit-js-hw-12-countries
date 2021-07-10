ed7752148c2ad4460ff86fe5f3766c7337b8265c
Evgeniy Stoyan <68951809+EvgeniyStoyan@users.noreply.github.com>
2021-07-10 15:39:08 +0300

    Добавляю ДЗ по 12 модулю - поиск стран

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import './sass/main.scss';
+import fetchCountries from './js/fetchCountries.js';
+import countryTemplate from './template/country.hbs';
+import countriesTemplate from './template/countries.hbs';
+const debounce = require('lodash.debounce');
+
+import '@pnotify/core/dist/BrightTheme.css';
+import { error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+
+const refs = {
+  inputText: document.querySelector('.input-text-js'),
+  countryCard: document.querySelector('.country-card'),
+};
+
+refs.inputText.addEventListener('input', debounce(searchCountry, 500));
+
+function searchCountry(event) {
+  event.preventDefault();
+  if (!event.target.value) {
+    refs.countryCard.innerHTML = '';
+    return;
+  }
+  const searchValue = event.target.value;
+  fetchCountries(searchValue).then(getCountries).catch(errorMessage);
+}
+
+function createMarkupCountry(country) {
+  country.map(country => {
+    refs.countryCard.innerHTML = countryTemplate(country);
+  });
+}
+
+function createMarkupCountries(countries) {
+  const selectionOfCountries = countries.map(country => country.name);
+  refs.countryCard.innerHTML = countriesTemplate(selectionOfCountries);
+}
+
+function getCountries(country) {
+  if (country.length === 1) {
+    createMarkupCountry(country);
+  } else if (country.length >= 2 && country.length <= 10) {
+    createMarkupCountries(country);
+  } else {
+    errorMessage();
+  }
+}
+
+function errorMessage() {
+  error({
+    text: 'Too many matches found. Please enter a more specific query.',
+  });
+}
