c2cdc413fda761197d1fc5c9dc8a00386ebcb763
Evgeniy Stoyan <68951809+EvgeniyStoyan@users.noreply.github.com>
2023-04-16 12:17:32 +0300

    edit

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,5 @@
 export default function fetchCountries(searchQuery) {
-  return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`).then(response => {
+  return fetch(`https://restcountries.com/v2/name/${searchQuery}`).then(response => {
     return response.json();
   });
 }
